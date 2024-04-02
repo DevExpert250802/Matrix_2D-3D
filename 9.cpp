81c2445da0053a524dd537523d7593a09a291a5a
Devansh Khandelwal <108886695+dev25082@users.noreply.github.com>
2024-04-02 23:17:19 +0530

    Median in a row-wise sorted MatrixMedian in a row-wise sorted Matrix

---
+++ 9.cpp
@@ -0,0 +1,18 @@
+Median in a row-wise sorted MatrixMedian in a row-wise sorted Matrix
+
+
+
+
+int median(vector<vector<int>> &matrix, int m, int n) {
+  vector<int> arr;
+  
+  //flatten the 2D matrix into a 1D Array as show below:
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < matrix[0].size(); j++) {
+      arr.push_back(matrix[i][j]);
+    }
+  }
+  sort(arr.begin(), arr.end());
+  return arr[m*n/2];
+}
+
